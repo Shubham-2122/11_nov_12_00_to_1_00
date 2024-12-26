890ed511dd6fc6e66e1571315895f2e300a5512c
Shubham-2122 <shubhamjadav.tops@gmail.com>
2024-12-26 12:32:13 +0530

    javscript

---
+++ Javascript/ex.js
@@ -0,0 +1,9 @@
+console.log("hello external javscript ");
+document.write("hello ")
+
+// bom browser window
+
+// window.alert("pls required filed!...");
+// confirm("Are your sure ? yes / no");
+// prompt("5 + 5 ? ")
+// print();
