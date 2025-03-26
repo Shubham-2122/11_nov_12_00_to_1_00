96c1ab2c393d4cf667ccdbc512a29286160981ff
Shubham-2122 <shubhamjadav.tops@gmail.com>
2025-03-26 12:31:23 +0530

    react

--- React/react-vite/src/App.jsx
+++ React/react-vite/src/App.jsx
@@ -1,10 +1,12 @@
 import React
  from "react"
+import Hello from "./Hello"
 function App() {
 
   return (
     <>
-      <h1>hello this App jsx</h1>
+      <h1>hello this App jsx 2</h1>
+      <Hello />
     </>
   )
 }
---
+++ React/react-vite/src/Hello.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Hello() {
+  return (
+    <div>
+      <h1>Heko h1 data 2</h1>
+    </div>
+  )
+}
+
+export default Hello
